aa1c27c30b9ac5f81b399b3306c4545d95a2b48c
joshua fordyce880 <jfordyce880@gmail.com>
2023-10-06 16:58:31 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,10 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Hichem Fantar',
+    title: 'Joshua Fordyce',
     description:
-      'Hichem Fantar is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://hichemfantar.me', // No trailing slash allowed!
+      'Joshua Fordyce is a cloud engineer at Oracle and an Aspiring machine learning researcher.',
+    siteUrl: 'https://JoshuaFordyce.me', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@hichemfantar',
   },
